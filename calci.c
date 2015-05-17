e21ddd38578c4f7e2cf4ff091d9c5b64e7fce46f
Aalisha Dalal <aalishadalal1996@gmail.com>
2015-05-17 10:50:25 +0530

    basic calculator functions

--- calci.c
+++ calci.c
@@ -1,1 +1,17 @@
-dwebwjcrefnlvelk
+#include <stdio.h>
+
+int main(){
+  int a;
+  int b;
+  add(a,b);
+  sub(a,b);
+}
+add(int x,int y){
+  return (x+y);
+}
+sub(int x,int y){
+  return(x-y);
+}
+mutiply(int x,int y){
+  return(x*y);
+}
